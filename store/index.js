5b7b0806c42ec8d99f9fad0f27bfaa0dc5eb9687
bai-code <1580087304@qq.com>
2022-12-08 22:58:59 +0800

    下一步

--- store/index.js
+++ store/index.js
@@ -1,9 +1,19 @@
 import { createStore } from 'vuex'
 
+const audio = uni.createInnerAudioContext()
+
 export default createStore({
 	state(){
 		return {
 			count:'asdfsdf'
+		}
+	},
+	mutations:{
+		play(){
+			audio.play()
+		},
+		pause(){
+			audio.pause()
 		}
 	}
 })
